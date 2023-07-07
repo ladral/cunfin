450720f9554523c8fd8ecfecaf431e599fa9e4b6
ladral <tuor_philipp@hotmail.com>
2023-07-08 00:58:21 +0200

    refactor: introduce package name to align with default linter rules

--- app.mjs
+++ app.mjs
@@ -1,13 +1,13 @@
-import grpc from '@grpc/grpc-js';
-import protoLoader from '@grpc/proto-loader';
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
 
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { fileURLToPath } from "url";
+import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-let PROTO_PATH = __dirname + '/protos/services/cunfin/v1/token_service.proto';
+let PROTO_PATH = __dirname + '/protos/cunfin/v1/token_service.proto';
 
 // Suggested options for similarity to existing grpc.load behavior
 let packageDefinition = protoLoader.loadSync(
@@ -19,7 +19,7 @@ let packageDefinition = protoLoader.loadSync(
         oneofs: true
     });
 
-let tokenProto = grpc.loadPackageDefinition(packageDefinition);
+let protoDescriptor  = grpc.loadPackageDefinition(packageDefinition);
 
 function introspect(call, callback) {
     callback(null, {active: true});
@@ -30,9 +30,10 @@ function introspect(call, callback) {
  */
 function main() {
     let server = new grpc.Server();
-    server.addService(tokenProto.TokenService.service, {introspect: introspect});
+    server.addService(protoDescriptor.cunfin.v1.TokenService.service, { introspect });
     server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
         server.start();
+        console.log('gRPC server started successfully.');
     });
 }
 
