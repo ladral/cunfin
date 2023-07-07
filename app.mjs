a3581d43d676d7100c2265ca0c2d10ae7dcb5c73
ladral <tuor_philipp@hotmail.com>
2023-07-08 00:18:19 +0200

    feat: init project

---
+++ app.mjs
@@ -0,0 +1,39 @@
+import grpc from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader';
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let PROTO_PATH = __dirname + '/protos/services/cunfin/v1/token_service.proto';
+
+// Suggested options for similarity to existing grpc.load behavior
+let packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+
+let tokenProto = grpc.loadPackageDefinition(packageDefinition);
+
+function introspect(call, callback) {
+    callback(null, {active: true});
+}
+
+/**
+ * Starts an RPC
+ */
+function main() {
+    let server = new grpc.Server();
+    server.addService(tokenProto.TokenService.service, {introspect: introspect});
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+        server.start();
+    });
+}
+
+main();
