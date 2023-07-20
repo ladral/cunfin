4ccbbbdde84adb891c48e01108e205b3194a287c
ladral <tuor_philipp@hotmail.com>
2023-07-20 22:09:02 +0200

    feat: add dummy token service

--- src/app.mjs
+++ src/app.mjs
@@ -1,4 +1,4 @@
-import { loadProtoDefinition } from "./infra/proto-loader-proxy.mjs";
+import {loadProtoDefinition} from "./infra/proto-loader-proxy.mjs";
 import grpc from "@grpc/grpc-js";
 
 const protoDefinition = loadProtoDefinition();
@@ -7,11 +7,19 @@ function introspect(call, callback) {
     callback(null, {active: true});
 }
 
+function generateToken(call, callback) {
+    callback(null, {access_token: "token for: " + call.request.subject, token_type: "Bearer"})
+}
+
 const server = new grpc.Server();
-server.addService(protoDefinition.cunfin.v1.TokenService.service, {introspect});
 
+/* register gRPC token service */
+server.addService(protoDefinition.cunfin.v1.TokenService.service, {
+    introspect,
+    generateToken
+});
 
- /* start gRPC server */
+/* start gRPC server */
 server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
     server.start();
     console.log('gRPC server started successfully.');
